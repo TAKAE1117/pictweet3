a2d7aaa5aa539a513ce6b9a0bc25ce1dd444608d
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-06-06 21:09:25 +0900

    considering name error

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,7 +28,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text)
   end
 
 end
